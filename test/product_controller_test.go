2c1db3cfd80c2909b14f659c36a20e18a1cbb2e1
Rafli Ramadhan <rafliramdhn@gmail.com>
2025-04-02 11:21:27 +0700

    finishing testing

--- test/product_controller_test.go
+++ test/product_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"golang-point-of-sales-system/app"
 	"golang-point-of-sales-system/controller"
+	"golang-point-of-sales-system/middleware"
 	"golang-point-of-sales-system/modules/products/domain/entity"
 	"golang-point-of-sales-system/modules/products/domain/repository"
 	"golang-point-of-sales-system/modules/products/domain/service"
@@ -19,7 +20,6 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
-	"github.com/julienschmidt/httprouter"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -47,7 +47,7 @@ func setupTestDB() *sqlx.DB {
 
 }
 
-func setupRouter(db *sqlx.DB) *httprouter.Router {
+func setupRouter(db *sqlx.DB) http.Handler {
 	validate := validator.New()
 	productRepository := repository.NewProductRepository(db)
 	productService := service.NewProductService(productRepository, validate)
@@ -55,7 +55,7 @@ func setupRouter(db *sqlx.DB) *httprouter.Router {
 
 	router := app.NewRouter(productController)
 
-	return router
+	return middleware.NewAuthMiddleware(router)
 }
 
 func truncateProduct(db *sqlx.DB) {
@@ -287,17 +287,131 @@ func TestGetProductFailed(t *testing.T) {
 }
 
 func TestDeleteProductSuccess(t *testing.T) {
+	db := setupTestDB()
+	truncateProduct(db)
+	router := setupRouter(db)
 
+	tx := db.MustBegin()
+	productRepository := repository.NewProductRepository(db)
+	product := productRepository.Save(context.Background(), entity.Product{
+		Kode_produk: "P-001",
+		Nama_produk: "Product 002",
+		Merk:        "Merk 001",
+		Harga_beli:  1000,
+		Harga_jual:  2000,
+		Stok:        10,
+	})
+	tx.Commit()
+
+	tRequest := httptest.NewRequest(http.MethodDelete, "http://localhost:3000/api/v1/product/delete/"+product.Id.String(), nil)
+	tRequest.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, tRequest)
+
+	response := recorder.Result()
+	assert.Equal(t, 200, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	assert.Equal(t, "OK", responseBody["status"])
 }
 
 func TestDeleteProductFailed(t *testing.T) {
+	db := setupTestDB()
+	truncateProduct(db)
+	router := setupRouter(db)
 
+	tRequest := httptest.NewRequest(http.MethodDelete, "http://localhost:3000/api/v1/product/delete/e09a5e7b-95ed-4889-8170-fa52ab33a9ea", nil)
+	tRequest.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, tRequest)
+
+	response := recorder.Result()
+	assert.Equal(t, 404, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+	assert.Equal(t, 404, int(responseBody["code"].(float64)))
+	assert.Equal(t, "NOT FOUND", responseBody["status"])
 }
 
 func TestListProductSuccess(t *testing.T) {
+	db := setupTestDB()
+	truncateProduct(db)
+	router := setupRouter(db)
 
+	tx := db.MustBegin()
+	productRepository := repository.NewProductRepository(db)
+	product := productRepository.Save(context.Background(), entity.Product{
+		Kode_produk: "P-001",
+		Nama_produk: "Product 002",
+		Merk:        "Merk 001",
+		Harga_beli:  1000,
+		Harga_jual:  2000,
+		Stok:        10,
+	})
+	product2 := productRepository.Save(context.Background(), entity.Product{
+		Kode_produk: "P-002",
+		Nama_produk: "Product 003",
+		Merk:        "Merk 003",
+		Harga_beli:  2000,
+		Harga_jual:  3000,
+		Stok:        80,
+	})
+	tx.Commit()
+
+	tRequest := httptest.NewRequest(http.MethodGet, "http://localhost:3000/api/v1/product/lists", nil)
+	tRequest.Header.Add("X-API-Key", "RAHASIA")
+
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, tRequest)
+
+	response := recorder.Result()
+	assert.Equal(t, 200, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	assert.Equal(t, "OK", responseBody["status"])
+
+	var products = responseBody["data"].([]interface{})
+
+	productResponse1 := products[0].(map[string]interface{})
+	productResponse2 := products[1].(map[string]interface{})
+
+	assert.Equal(t, product.Id, uuid.MustParse(productResponse1["id"].(string)))
+	assert.Equal(t, product.Nama_produk, productResponse1["nama_produk"])
+
+	assert.Equal(t, product2.Id, uuid.MustParse(productResponse2["id"].(string)))
+	assert.Equal(t, product2.Nama_produk, productResponse2["nama_produk"])
 }
 
 func TestUnauthorized(t *testing.T) {
+	db := setupTestDB()
+	truncateProduct(db)
+	router := setupRouter(db)
 
+	tRequest := httptest.NewRequest(http.MethodGet, "http://localhost:3000/api/v1/product/lists", nil)
+	// tRequest.Header.Add("X-API-Key", "SALAH")
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, tRequest)
+
+	response := recorder.Result()
+	assert.Equal(t, 401, response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody map[string]interface{}
+	json.Unmarshal(body, &responseBody)
+	assert.Equal(t, 401, int(responseBody["code"].(float64)))
+	assert.Equal(t, "UNAUTHORIZED", responseBody["status"])
 }
