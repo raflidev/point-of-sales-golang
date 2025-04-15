07a1111300940e7e0036b8068d43db9202456d7a
Rafli Ramadhan <rafliramdhn@gmail.com>
2025-04-15 13:47:25 +0700

    add productlog

--- db/migrations/000001_create_products_table.up.sql
+++ db/migrations/000001_create_products_table.up.sql
@@ -4,9 +4,11 @@ CREATE TABLE product
   kode_produk VARCHAR(255) NULL,
   nama_produk VARCHAR(255) NULL,
   merk VARCHAR(255) NULL,
-  harga_beli VARCHAR(255) NULL,
-  diskon VARCHAR(255) NULL,
-  harga_jual VARCHAR(255) NULL,
-  stok VARCHAR(255) NULL,
+  harga_beli INT NULL,
+  diskon INT NULL,
+  harga_jual INT NULL,
+  stok INT NULL,
+  create_at TIMESTAMP DEFAULT now(),
+  update_at TIMESTAMP DEFAULT now(),
   PRIMARY KEY(id) 
 )
--- test/gorm_test.go
+++ test/gorm_test.go
@@ -3,16 +3,20 @@ package test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func OpenConnection() *gorm.DB {
-	dsn := "user=postgres dbname=point-of-sales-golang sslmode=disable password=postgres host=localhost"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := "user=postgres dbname=point-of-sales-golang-test sslmode=disable password=postgres host=localhost"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 
 	if err != nil {
 		panic(err)
@@ -32,13 +36,27 @@ func TestDB(t *testing.T) {
 }
 
 type Product struct {
-	Id          uuid.UUID
-	Kode_produk string
-	Nama_produk string
-	Merk        string
-	Harga_beli  int
-	Harga_jual  int
-	Stok        int
+	Id          uuid.UUID `gorm:"primary_key;column:id;type:uuid;default:uuid_generate_v4()"`
+	Kode_produk string    `gorm:"column:kode_produk;type:varchar(50);unique;not null"`
+	Nama_produk string    `gorm:"column:nama_produk;type:varchar(50);not null"`
+	Merk        string    `gorm:"column:merk;type:varchar(50);not null"`
+	Harga_beli  int       `gorm:"column:harga_beli;type:int;not null"`
+	Harga_jual  int       `gorm:"column:harga_jual;type:int;not null"`
+	Stok        int       `gorm:"column:stok;type:int;not null"`
+	CreateAt    time.Time `gorm:"column:create_at;autoCreateTime;default:now()"`
+	UpdateAt    time.Time `gorm:"column:update_at;autoCreateTime;default:now()"`
+}
+
+func (p *Product) TableName() string {
+	return "product"
+}
+
+type ProductLog struct {
+	ID        uuid.UUID `gorm:"primary_key;column:id;type:uuid;default:uuid_generate_v4()"`
+	ProductID uuid.UUID `gorm:"column:product_id;type:uuid;not null"`
+	Action    string    `gorm:"column:action;type:varchar(50);not null"`
+	CreateAt  time.Time `gorm:"column:create_at;autoCreateTime;default:now()"`
+	UpdateAt  time.Time `gorm:"column:update_at;autoCreateTime;default:now()"`
 }
 
 func TestExecuteSQL(t *testing.T) {
@@ -78,9 +96,11 @@ func TestSqlRow(t *testing.T) {
 		var Harga_beli int
 		var Harga_jual int
 		var Stok int
+		var CreateAt time.Time
+		var UpdateAt time.Time
 
 		fmt.Println(rows)
-		err := rows.Scan(&Id, &Kode_produk, &Nama_produk, &Merk, &Harga_beli, &Harga_jual, &Stok)
+		err := rows.Scan(&Id, &Kode_produk, &Nama_produk, &Merk, &Harga_beli, &Harga_jual, &Stok, &CreateAt, &UpdateAt)
 		assert.Nil(t, err)
 
 		products = append(products, Product{
@@ -91,9 +111,44 @@ func TestSqlRow(t *testing.T) {
 			Harga_beli:  Harga_beli,
 			Harga_jual:  Harga_jual,
 			Stok:        Stok,
+			CreateAt:    CreateAt,
+			UpdateAt:    UpdateAt,
 		})
 	}
 
 	assert.Equal(t, 1, len(products))
 
+}
+
+func TestCreateUser(t *testing.T) {
+	product := Product{
+		Kode_produk: "P-01",
+		Nama_produk: "Momogi",
+		Merk:        "Momogi",
+		Harga_beli:  1000,
+		Harga_jual:  1500,
+		Stok:        10,
+	}
+
+	response := db.Create(&product)
+	assert.Nil(t, response.Error)
+	assert.Equal(t, int64(1), response.RowsAffected)
+}
+
+func TestUpdateUser(t *testing.T) {
+	product := Product{}
+	err := db.First(&product, "id = ?", "0853420c-e23e-439a-a956-3df32204e8b6").Error
+	assert.Nil(t, err)
+
+	product.Nama_produk = "Momogi Baru"
+	product.Harga_beli = 3000
+
+	err = db.Save(&product).Error
+	assert.Nil(t, err)
+
+	result := db.Where("id = ?", "0853420c-e23e-439a-a956-3df32204e8b6").Updates(Product{
+		Nama_produk: "Momogi Baru",
+		Harga_beli:  3001,
+	})
+	assert.Nil(t, result.Error)
 }
